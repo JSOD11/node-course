02d888a6375318145aa98d8d1f4ae5cbd80714c6
Justin O'Dwyer <jsod@dhcp-10-250-171-227.harvard.edu>
2023-03-13 16:28:19 -0400

    added authorization for users

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,12 +6,22 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// CRUD: Create Read Update Delete
-//       POST   GET  PATCH  DELETE
+// app.use((req, res, next) => { // middleware example
+//   if (req.method === 'GET') {
+//     res.send('GET requests are disabled')
+//   } else {
+//     next() // this is necessary to continue the chain
+//   }
+// })
 
 app.use(express.json()) // use json
 app.use(userRouter)
 app.use(taskRouter)
+
+// CRUD: Create Read Update Delete
+//       POST   GET  PATCH  DELETE
+
+// with middleware: new request --> do something --> run route handler
 
 app.listen(port, () => { // express runs server and listens for requests
   console.log('Server is running on port ' + port)
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 // async function allows for await
@@ -7,37 +8,49 @@ router.post('/users', async (req, res) => { // POST == Create
   const user = new User(req.body)
 
   try {
-    await user.save() // instead of save, then, we just await
-    res.status(201).send(user)
+    const token = await user.generateAuthToken()
+    await user.save() // instead of save().then(), we just await
+    res.status(201).send({ user, token })
   } catch (e) {
     res.status(400).send(e)
   }
 })
 
-router.get('/users', async (req, res) => { // GET == read
-
+router.post('/users/login', async (req, res) => {
   try {
-    const users = await User.find()
-    res.status(201).send(users)
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
   } catch (e) {
-    res.status(500).send(e)
+    res.status(400).send()
   }
 })
 
-router.get('/users/:id', async (req, res) => { // GET == read
-
+router.post('/users/logout', auth, async (req, res) => {
   try {
-    const user = await User.findById(req.params.id)
-    if (!user) {
-      return res.status(404).send()
-    }
-    res.status(200).send(user)
+    req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
+    await req.user.save()
+    res.send()
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send()
   }
 })
 
-router.patch('/users/:id', async (req, res) => {
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = []
+    await req.user.save()
+    res.send()
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.get('/users/me', auth, async (req, res) => { // GET == read
+  res.send(req.user)
+})
+
+router.patch('/users/me', auth, async (req, res) => { // patch == update
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -47,18 +60,17 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-    if (!user) { return res.status(404).send() }
-    res.send(user)
+    updates.forEach((update) => req.user[update] = req.body[update])
+    await req.user.save()
+    res.send(req.user)
   } catch (e) {
     res.status(400).send(e)
   }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id)
-
+    const user = await User.findByIdAndDelete(req.user._id)
     if (!user) { return res.status(404).send() }
     res.status(200).send(user)
   } catch (e) {
